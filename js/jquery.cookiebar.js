186e46ed6235d5cf1b9f89a5808b97f2b55d2c66
NIklasjo <153642660+NIklasjo@users.noreply.github.com>
2023-12-13 10:23:38 +0100

    Update jquery.cookiebar.js

--- js/jquery.cookiebar.js
+++ js/jquery.cookiebar.js
@@ -25,22 +25,22 @@
 		var defaults = {
 			message: 'Vi använder cookies för att spåra användning och preferenser.', //Message displayed on bar
 			acceptButton: true, //Set to true to show accept/enable button
-			acceptText: 'I Understand', //Text on accept/enable button
+			acceptText: 'Acceptera', //Text on accept/enable button
 			acceptFunction: function(cookieValue){if(cookieValue!='enabled' && cookieValue!='accepted') window.location = window.location.href;}, //Function to run after accept
-			declineButton: false, //Set to true to show decline/disable button
-			declineText: 'Disable Cookies', //Text on decline/disable button
+			declineButton: true, //Set to true to show decline/disable button
+			declineText: 'Avböj', //Text on decline/disable button
 			declineFunction: function(cookieValue){if(cookieValue=='enabled' || cookieValue=='accepted') window.location = window.location.href;}, //Function to run after decline
-			policyButton: false, //Set to true to show Privacy Policy button
-			policyText: 'Privacy Policy', //Text on Privacy Policy button
+			policyButton: true, //Set to true to show Privacy Policy button
+			policyText: 'Integritetspolicy', //Text on Privacy Policy button
 			policyURL: '/privacy-policy/', //URL of Privacy Policy
-			autoEnable: true, //Set to true for cookies to be accepted automatically. Banner still shows
+			autoEnable: false, //Set to true for cookies to be accepted automatically. Banner still shows
 			acceptOnContinue: false, //Set to true to accept cookies when visitor moves to another page
 			acceptOnScroll: false, //Set to true to accept cookies when visitor scrolls X pixels up or down
 			acceptAnyClick: false, //Set to true to accept cookies when visitor clicks anywhere on the page
-			expireDays: 365, //Number of days for cookieBar cookie to be stored for
+			expireDays: 1825, //Number of days for cookieBar cookie to be stored for
 			renewOnVisit: false, //Renew the cookie upon revisit to website
 			forceShow: false, //Force cookieBar to show regardless of user cookie preference
-			effect: 'slide', //Options: slide, fade, hide
+			effect: 'fade', //Options: slide, fade, hide
 			element: 'body', //Element to append/prepend cookieBar to. Remember "." for class or "#" for id.
 			append: false, //Set to true for cookieBar HTML to be placed at base of website. Actual position may change according to CSS
 			fixed: false, //Set to true to add the class "fixed" to the cookie bar. Default CSS should fix the position
