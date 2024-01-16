256142e7203cb548d9b2e5d71bce6e8080b7d2e2
NIklasjo <153642660+NIklasjo@users.noreply.github.com>
2024-01-16 10:17:56 +0100

    Update Om-KNS-Consulting-AB.js

--- js/Om-KNS-Consulting-AB.js
+++ js/Om-KNS-Consulting-AB.js
@@ -1,4 +1,12 @@
-   
+var elementCarousel1 = document.querySelector('#Carousel1');
+if (elementCarousel1) {
+    elementCarousel1.addEventListener('slid.bs.carousel', function(e) {
+        var el = document.getElementById('LayoutGrid2');
+   if (el.addEventListener)
+   {
+      el.addEventListener('touchmove', ontouchmoveLayoutGrid2);
+   }
+    });   
    document.addEventListener('DOMContentLoaded', function(event)
    {
       var elementCarousel2 = document.querySelector('#Carousel2');
@@ -61,15 +69,7 @@
      })();
    }
    
-        var elementCarousel1 = document.querySelector('#Carousel1');
-if (elementCarousel1) {
-    elementCarousel1.addEventListener('slid.bs.carousel', function(e) {
-        var el = document.getElementById('LayoutGrid2');
-   if (el.addEventListener)
-   {
-      el.addEventListener('touchmove', ontouchmoveLayoutGrid2);
-   }
-    });
+        
    function ontouchmoveLayoutGrid2(e)
    {
       AnimationResume('Card1');
