c8ea36a5ac359e5fadb9cac779e63cefbe70a0cd
NIklasjo <153642660+NIklasjo@users.noreply.github.com>
2025-04-13 22:05:33 +0200

    Update page4.js

--- js/page4.js
+++ js/page4.js
@@ -1,16 +1,7 @@
-// Sätt upp länken för Google Slides-publicering
-const googleSlidesUrl = "https://docs.google.com/presentation/d/e/2PACX-1vRi-AkpEvBey5vRYncDsIq-yLJcEd9ziAHt2KA-9jPCuVlHfbmb1o3HF3_--FcuMWudWyUi9qh-OscW/pub?start=false&loop=false&delayms=3000&slide=id.p";
-
-// Funktion för att ladda Google Slides och visa det i div
-function loadGoogleSlides() {
-    const container = document.getElementById('google-slides-container');
-    container.innerHTML = `<object data="${googleSlidesUrl}" type="application/x-shockwave-flash" width="100%" height="100%">
-                             <param name="movie" value="${googleSlidesUrl}">
-                           </object>`;
+function refreshSlide() {
+  const iframe = document.getElementById('slide-frame');
+  const baseUrl = "https://docs.google.com/presentation/d/e/2PACX-1vRi-AkpEvBey5vRYncDsIq-yLJcEd9ziAHt2KA-9jPCuVlHfbmb1o3HF3_--FcuMWudWyUi9qh-OscW/pub?start=false&loop=false&delayms=3000&slide=id.p";
+  const timestamp = new Date().getTime();
+  window.location.href = baseUrl + "&t=" + timestamp;
 }
-
-// Ladda initialt och sätt intervall för att uppdatera var 20:e sekund
-loadGoogleSlides();
-setInterval(loadGoogleSlides, 20000); // 20000 millisekunder = 20 sekunder
-
-               
+setInterval(refreshSlide, 20000);
