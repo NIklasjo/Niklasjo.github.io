f837d93ea9f311b2521e5591293273ad0ffccf39
NIklasjo <153642660+NIklasjo@users.noreply.github.com>
2025-04-14 07:21:42 +0200

    Update page4.js

--- js/page4.js
+++ js/page4.js
@@ -1,7 +1,1 @@
-function refreshSlide() {
-  const iframe = document.getElementById('slide-frame');
-  const baseUrl = "https://docs.google.com/presentation/d/e/2PACX-1vRi-AkpEvBey5vRYncDsIq-yLJcEd9ziAHt2KA-9jPCuVlHfbmb1o3HF3_--FcuMWudWyUi9qh-OscW/pub?start=false&loop=false&delayms=3000&slide=id.p";
-  const timestamp = new Date().getTime();
-  window.location.href = baseUrl + "&t=" + timestamp;
-}
-setInterval(refreshSlide, 20000);
+
