ef8e24beae213973973d1c0f75b7204c67e43824
NIklasjo <153642660+NIklasjo@users.noreply.github.com>
2025-04-25 21:39:14 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,9 @@
-
 const CACHE_NAME = 'signage-cache-v1';
-const URLS_TO_CACHE = [
-  '/',
-  '/index.html',
-  '/sw.js',
-  'https://docs.google.com/presentation/d/e/2PACX-1vRi-AkpEvBey5vRYncDsIq-yLJcEd9ziAHt2KA-9jPCuVlHfbmb1o3HF3_--FcuMWudWyUi9qh-OscW/embed?start=true&loop=true&delayms=25000&rm=minimal',
-  'https://weatherwidget.io/js/widget.min.js'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE)));
+const URLS_TO_CACHE = [ '/', '/index.html', '/slides_fallback.html', '/sw.js', 'https://weatherwidget.io/js/widget.min.js' ];
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(URLS_TO_CACHE))));
+self.addEventListener('fetch', e => {
+  const req = e.request;
+  if (req.url.includes('docs.google.com')) e.respondWith(fetch(req).catch(() => caches.match('/slides_fallback.html')));
+  else                          e.respondWith(fetch(req).catch(() => caches.match(req)));
 });
-
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-    ))
-  );
-});
+self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k!==CACHE_NAME).map(k => caches.delete(k))))));
